eb569ff12a52466c8b23caee7e8d654fd99fcc82
Алексей <aleksejkiselev831@gmail.com>
2025-08-18 17:42:54 +0300

    Add files via upload

---
+++ search_engine/tests/server_tests.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+
+#include "../include/search_server.h"
+
+TEST(TestCaseSearchServer, TestSimple) {
+    const std::vector<std::string> docs = {
+        "milk milk milk milk water water water",
+        "milk water water",
+        "milk milk milk milk milk water water water water water",
+        "americano cappuccino"
+    };
+    const std::vector<std::string> request = {"milk water", "sugar"};
+    const std::vector<std::vector<RelativeIndex>> expected = {
+        {
+            {2, 1.0f},
+            {0, 0.7f},
+            {1, 0.3f}
+        },
+        {}
+    };
+    InvertedIndex idx;
+    idx.UpdateDocumentBase(docs);
+    SearchServer srv(idx);
+    auto result = srv.search(request);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(TestCaseSearchServer, TestTop5) {
+    const std::vector<std::string> docs = {
+        "london is the capital of great britain",
+        "paris is the capital of france",
+        "berlin is the capital of germany",
+        "rome is the capital of italy",
+        "madrid is the capital of spain",
+        "lisbon is the capital of portugal",
+        "bern is the capital of switzerland",
+        "moscow is the capital of russia",
+        "kiev is the capital of ukraine",
+        "minsk is the capital of belarus",
+        "astana is the capital of kazakhstan",
+        "beijing is the capital of china",
+        "tokyo is the capital of japan",
+        "bangkok is the capital of thailand",
+        "welcome to moscow the capital of russia the third rome",
+        "amsterdam is the capital of netherlands",
+        "helsinki is the capital of finland",
+        "oslo is the capital of norway",
+        "stockholm is the capital of sweden",
+        "riga is the capital of latvia",
+        "tallinn is the capital of estonia",
+        "warsaw is the capital of poland"
+    };
+    const std::vector<std::string> request = {"moscow is the capital of russia"};
+    const std::vector<std::vector<RelativeIndex>> expected = {
+        {
+            {7, 1.0f},
+            {14, 1.0f},
+            {0, 0.666666687f},
+            {1, 0.666666687f},
+            {2, 0.666666687f}
+        }
+    };
+    InvertedIndex idx;
+    idx.UpdateDocumentBase(docs);
+    SearchServer srv(idx);
+    auto result = srv.search(request);
+    ASSERT_EQ(result, expected);
+}
